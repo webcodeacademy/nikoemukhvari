a058a3444d6558b4a902344611aeed93a6fe2cce
webcodeacademy <webcodeacademyinfo@gmail.com>
2024-06-13 11:10:31 +0400

    Furniture Store

---
+++ script/pliusminus1.js
@@ -0,0 +1,282 @@
+let pliusminus1 = (s)=> {
+
+    // gavigot minuss daawva tu pliuss
+    
+    
+    let op = s.innerHTML;
+    let number = parseInt(document.getElementById("pirveli").innerHTML);
+
+
+    console.log(op, number);
+    if(op == '−'){
+        number--;
+    }
+    else{
+        number++;
+    }
+
+
+document.getElementById("pirveli").innerHTML = number;
+
+
+}
+
+
+
+
+let pliusminus2 = (s)=> {
+
+    // gavigot minuss daawva tu pliuss
+    
+    
+    let op = s.innerHTML;
+    let number = parseInt(document.getElementById("meore").innerHTML);
+
+
+    console.log(op, number);
+    if(op == '−'){
+        number--;
+    }
+    else{
+        number++;
+    }
+
+
+document.getElementById("meore").innerHTML = number;
+
+
+}
+
+
+
+
+
+let pliusminus3 = (s)=> {
+
+    // gavigot minuss daawva tu pliuss
+    
+    
+    let op = s.innerHTML;
+    let number = parseInt(document.getElementById("mesame").innerHTML);
+
+
+    console.log(op, number);
+    if(op == '−'){
+        number--;
+    }
+    else{
+        number++;
+    }
+
+
+document.getElementById("mesame").innerHTML = number;
+
+
+}
+
+
+
+
+let pliusminus4 = (s)=> {
+
+    // gavigot minuss daawva tu pliuss
+    
+    
+    let op = s.innerHTML;
+    let number = parseInt(document.getElementById("meotxe").innerHTML);
+
+
+    console.log(op, number);
+    if(op == '−'){
+        number--;
+    }
+    else{
+        number++;
+    }
+
+
+document.getElementById("meotxe").innerHTML = number;
+
+
+}
+
+
+
+
+
+let pliusminus5 = (s)=> {
+
+    // gavigot minuss daawva tu pliuss
+    
+    
+    let op = s.innerHTML;
+    let number = parseInt(document.getElementById("mexute").innerHTML);
+
+
+    console.log(op, number);
+    if(op == '−'){
+        number--;
+    }
+    else{
+        number++;
+    }
+
+
+document.getElementById("mexute").innerHTML = number;
+
+
+}
+
+
+
+let pliusminus6 = (s)=> {
+
+    // gavigot minuss daawva tu pliuss
+    
+    
+    let op = s.innerHTML;
+    let number = parseInt(document.getElementById("meeqvse").innerHTML);
+
+
+    console.log(op, number);
+    if(op == '−'){
+        number--;
+    }
+    else{
+        number++;
+    }
+
+
+document.getElementById("meeqvse").innerHTML = number;
+
+
+}
+
+
+
+let pliusminus7 = (s)=> {
+
+    // gavigot minuss daawva tu pliuss
+    
+    
+    let op = s.innerHTML;
+    let number = parseInt(document.getElementById("meshvide").innerHTML);
+
+
+    console.log(op, number);
+    if(op == '−'){
+        number--;
+    }
+    else{
+        number++;
+    }
+
+
+document.getElementById("meshvide").innerHTML = number;
+
+
+}
+
+
+
+
+let pliusminus8 = (s)=> {
+
+    // gavigot minuss daawva tu pliuss
+    
+    
+    let op = s.innerHTML;
+    let number = parseInt(document.getElementById("merve").innerHTML);
+
+
+    console.log(op, number);
+    if(op == '−'){
+        number--;
+    }
+    else{
+        number++;
+    }
+
+
+document.getElementById("merve").innerHTML = number;
+
+
+}
+
+
+
+
+let pliusminus9 = (s)=> {
+
+    // gavigot minuss daawva tu pliuss
+    
+    
+    let op = s.innerHTML;
+    let number = parseInt(document.getElementById("mecxre").innerHTML);
+
+
+    console.log(op, number);
+    if(op == '−'){
+        number--;
+    }
+    else{
+        number++;
+    }
+
+
+document.getElementById("mecxre").innerHTML = number;
+
+
+}
+
+
+
+
+
+let pliusminus10 = (s)=> {
+
+    // gavigot minuss daawva tu pliuss
+    
+    
+    let op = s.innerHTML;
+    let number = parseInt(document.getElementById("meate").innerHTML);
+
+
+    console.log(op, number);
+    if(op == '−'){
+        number--;
+    }
+    else{
+        number++;
+    }
+
+
+document.getElementById("meate").innerHTML = number;
+
+
+}
+
+
+
+let pliusminus11 = (s)=> {
+
+    // gavigot minuss daawva tu pliuss
+    
+    
+    let op = s.innerHTML;
+    let number = parseInt(document.getElementById("metertmete").innerHTML);
+
+
+    console.log(op, number);
+    if(op == '−'){
+        number--;
+    }
+    else{
+        number++;
+    }
+
+
+document.getElementById("metertmete").innerHTML = number;
+
+
+}
